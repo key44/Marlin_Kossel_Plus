24ff8c3cfee5c1afe3761f75b7f07e2e03a009bc
key44 <gloomy.kuro@gmail.com>
2020-03-26 16:58:02 +0900

    マザーボードをramps1.4に変更

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -146,7 +146,8 @@
 
 // Choose the name from boards.h that matches your setup
 #ifndef MOTHERBOARD
-  #define MOTHERBOARD BOARD_TRIGORILLA_14
+  //#define MOTHERBOARD BOARD_TRIGORILLA_14
+  #define MOTHERBOARD BOARD_RAMPS_14_EFB
 #endif
 
 // Name displayed in the LCD "Ready" message and Info menu
